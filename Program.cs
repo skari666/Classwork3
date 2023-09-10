020eb7ef0957073a92a3d89a533b7ca8bf813997
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-10 20:57:37 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string st = Console.ReadLine();
+int st1, st2, st3;
+bool prov = int.TryParse(st, out st1);
+if (prov)
+{
+    st2 = st1 + 1;
+    st3 = st1 - 1;
+    Console.WriteLine($"The next number for the number {st1} is {st2}");
+    Console.WriteLine($"The previous number for the number {st1} is {st3}");
+}
+else
+{
+    Console.WriteLine("Это не число");
+}
